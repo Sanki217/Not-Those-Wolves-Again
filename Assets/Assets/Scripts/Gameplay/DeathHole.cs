878eb0b78523152ec19e1b0252fdd95a9b49b4b5
Sanki <michal.sanki@gmail.com>
2024-11-05 20:37:52 +0100

    hell and heaven, game updates

--- Assets/Assets/Scripts/Gameplay/DeathHole.cs
+++ Assets/Assets/Scripts/Gameplay/DeathHole.cs
@@ -1,29 +1,38 @@
 using UnityEngine;
 using UnityEngine.VFX;
+using System.Collections;
 
 public class DeathHole : MonoBehaviour
 {
-    public AudioSource hellAudio;     
-    public AudioSource heavenAudio;          
-    public VisualEffect hellVFX;      
-    public VisualEffect heavenVFX;           
+    public AudioSource hellAudio;
+    public AudioSource heavenAudio;
+    public VisualEffect hellVFX;
+    public VisualEffect heavenVFX;
+    public float effectDelay = 0.5f;  // Delay in seconds before effects start
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Wolf") || other.CompareTag("Sheep"))
         {
-            hellAudio.Play();
-            hellVFX.Play();
+            StartCoroutine(PlayHellEffectsWithDelay());
         }
-        
         else if (other.CompareTag("Player"))
         {
-            
-            heavenAudio.Play();
-            heavenVFX.Play();
-            
-
-           
+            StartCoroutine(PlayHeavenEffectsWithDelay());
         }
+    }
+
+    private IEnumerator PlayHellEffectsWithDelay()
+    {
+        yield return new WaitForSeconds(effectDelay); // Wait for the specified delay
+        hellAudio.Play();
+        hellVFX.Play();
+    }
+
+    private IEnumerator PlayHeavenEffectsWithDelay()
+    {
+        yield return new WaitForSeconds(effectDelay); // Wait for the specified delay
+        heavenAudio.Play();
+        heavenVFX.Play();
     }
 }
